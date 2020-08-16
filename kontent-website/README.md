9c84f8a9a219b15d40b452cb064470094a859b02
Ivan Sanchez <ivan@gourame.com>
2020-08-16 18:19:38 +0100

    Add new module for website

---
+++ kontent-website/README.md
@@ -0,0 +1,2 @@
+# Kontent website
+
---
+++ kontent-website/src/main/kotlin/Serve.kt
@@ -0,0 +1,8 @@
+import org.github.s4nchez.Kontent
+import org.github.s4nchez.asHttpHandler
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
+fun main() {
+    Kontent().build().asHttpHandler().asServer(SunHttp(8000)).start()
+}
