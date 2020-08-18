18bfa54a582c9ba421921a23636526f59e853f10
Ivan Sanchez <ivan@gourame.com>
2020-08-18 14:39:44 +0100

    Update website

--- kontent-website/publish.sh
+++ kontent-website/publish.sh
@@ -4,6 +4,6 @@ PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 $PROJECT_ROOT/gradlew buildWebsite
 
-git add docs/*
+git add $PROJECT_ROOT/docs/*
 git commit -am"Update website"
 git push
