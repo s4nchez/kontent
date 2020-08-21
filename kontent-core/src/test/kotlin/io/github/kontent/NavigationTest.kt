869a0c8c9cbdcdd87288bc4a8abf1334d4c1f300
Ivan Sanchez <ivan@gourame.com>
2020-08-21 14:23:36 +0100

    Handle intermediate nav items

--- kontent-core/src/test/kotlin/io/github/kontent/NavigationTest.kt
+++ kontent-core/src/test/kotlin/io/github/kontent/NavigationTest.kt
@@ -35,11 +35,11 @@ internal class NavigationTest {
 
         assertThat(site.generateNavigation(),
             equalTo(Navigation(listOf(
-                NavigationItem("About", Uri.of("/about"), listOf(
+                NavigationItem("About", null, listOf(
                     NavigationItem("Code of conduct", Uri.of("/about/code-of-conduct")),
                     NavigationItem("Contact", Uri.of("/about/contact"))
                 )),
-                NavigationItem("Docs", Uri.of("/docs"), listOf(
+                NavigationItem("Docs", null, listOf(
                     NavigationItem("Bar", Uri.of("/docs/bar")),
                     NavigationItem("Foo", Uri.of("/docs/foo"))
                 ))
@@ -55,7 +55,6 @@ internal class NavigationTest {
 }
 
 
-
 data class Navigation(val items: List<NavigationItem>)
 
 data class NavigationItem(val name: String, val uri: Uri?, val children: List<NavigationItem> = listOf())
@@ -64,35 +63,37 @@ private fun List<Page>.toNavigation(): Navigation {
     val rootItems =
         filterNot { it.uri.path.replace("/", "").isBlank() }
             .sortedBy { it.uri.path }
-            .map { it to NavigationItem(it.uri.path.name(), it.uri) }
+            .map { PagePath(it.uri.path, it.uri) to NavigationItem(it.uri.path.name(), it.uri) }
             .toMap()
             .aggregateChildren()
-            .map(Map.Entry<Page, NavigationItem>::value)
+            .map(Map.Entry<PagePath, NavigationItem>::value)
             .toList()
     return Navigation(rootItems)
 }
 
-private fun Site.generateNavigation(): Navigation = pages.toNavigation()
-private fun Page.segments() = uri.path.split("/").filterNot(String::isBlank)
-private fun String.name() = split("/").filterNot(String::isBlank).last().capitalize().replace("-", " ")
-private fun Page.parent() = "/" + uri.path.split("/").filterNot(String::isBlank).dropLast(1).joinToString("/")
+data class PagePath(val path: String, val target: Uri? = null)
 
-private fun Map<Page, NavigationItem>.aggregateChildren(): Map<Page, NavigationItem> =
+private fun Site.generateNavigation(): Navigation = pages.toNavigation()
+private fun PagePath.segments() = path.split("/").filterNot(String::isBlank)
+private fun String.name() = split("/").filterNot(String::isBlank).last().capitalize().replace("-", " ")
+private fun PagePath.parent() = "/" + path.split("/").filterNot(String::isBlank).dropLast(1).joinToString("/")
+
+private fun Map<PagePath, NavigationItem>.aggregateChildren(): Map<PagePath, NavigationItem> =
     if (keys.map { it.segments().size }.max() == 1)
         this
     else
-        entries.fold(mapOf<Page, NavigationItem>()) { acc, d: Map.Entry<Page, NavigationItem> ->
+        entries.fold(mapOf<PagePath, NavigationItem>()) { acc, d: Map.Entry<PagePath, NavigationItem> ->
             val (path, nextItem) = d
             if (path.segments().size == 1)
                 acc + (path to nextItem)
             else {
                 val parent = path.parent()
-                acc + (Page(Uri.of(parent), Html("dummy")) to acc.findOrCreateParent(parent).addChild(nextItem))
+                acc + (PagePath(parent) to acc.findOrCreateParent(parent).addChild(nextItem))
             }
         }.aggregateChildren()
 
-private fun Map<Page, NavigationItem>.findOrCreateParent(parent: String): NavigationItem =
-    keys.find { it.uri.path == parent }?.let { get(it) } ?: NavigationItem(parent.name(), Uri.of(parent))
+private fun Map<PagePath, NavigationItem>.findOrCreateParent(parent: String): NavigationItem =
+    keys.find { it.path == parent }?.let { get(it) } ?: NavigationItem(parent.name(), null)
 
 private fun NavigationItem.addChild(navigationItem: NavigationItem) =
     copy(children = children + navigationItem)
