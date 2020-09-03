7ae9a8dab4d0574ec8e80ecfa6cce89e5ee8bdd8
Ivan Sanchez <ivan@gourame.com>
2020-09-03 14:56:24 +0100

    Extract MarkdownSourceContract

---
+++ kontent-core/src/test/kotlin/io/github/kontent/markdown/MarkdownSourceContract.kt
@@ -0,0 +1,30 @@
+package io.github.kontent.markdown
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.core.Uri
+import org.junit.jupiter.api.Test
+
+abstract class MarkdownSourceContract {
+    protected val testFiles = listOf(
+        "/readme.md",
+        "/irrelevant/bob.md",
+        "/www/index.md",
+        "/www/sub/page.md"
+    )
+
+    protected abstract val source: MarkdownSource
+
+    protected abstract fun sourceUriFor(relativePath: String): Uri
+
+    @Test
+    fun `list all markdown files`() {
+        val files = source.listAllSources()
+
+        assertThat(files.toSet(), equalTo(setOf(
+            MarkdownSourceFile(sourceUriFor("/www/index.md"), Uri.of("/index")),
+            MarkdownSourceFile(sourceUriFor("/www/sub/page.md"), Uri.of("/sub/page"))
+        )))
+    }
+}
+
