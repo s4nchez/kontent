0ae8de339fe44fc065e18babddb6f67d7b8a7edd
Ivan Sanchez <ivan@gourame.com>
2020-09-03 14:54:15 +0100

    Introduce MarkdownSource interface

--- kontent-core/src/main/kotlin/io/github/kontent/markdown/FileSystemMarkdownSource.kt
+++ kontent-core/src/main/kotlin/io/github/kontent/markdown/FileSystemMarkdownSource.kt
@@ -5,12 +5,17 @@ import io.github.kontent.resolvePageUri
 import org.http4k.core.Uri
 import java.io.File
 
-class FileSystemMarkdownSource(private val config: SiteConfiguration) {
+interface MarkdownSource {
+    fun listAllSources(): Sequence<MarkdownSourceFile>
+    fun read(sourceFile: MarkdownSourceFile): Markdown
+}
 
-    fun listAllSources(): Sequence<MarkdownSourceFile> = File(config.sourcePath.value).walkTopDown()
+class FileSystemMarkdownSource(private val config: SiteConfiguration) : MarkdownSource {
+
+    override fun listAllSources(): Sequence<MarkdownSourceFile> = File(config.sourcePath.value).walkTopDown()
         .filter { it.name.endsWith(".md") }
         .map { MarkdownSourceFile(Uri.of(it.path), it.resolvePageUri(config)) }
         .plus(config.standalonePages.map { MarkdownSourceFile(Uri.of(it.sourcePath), it.uri) })
 
-    fun read(sourceFile: MarkdownSourceFile): Markdown = Markdown(File(sourceFile.location.path).readText())
+    override fun read(sourceFile: MarkdownSourceFile): Markdown = Markdown(File(sourceFile.location.path).readText())
 }
