24d21fc6829085191d2448f1f016a5d11f81952f
Ivan Sanchez <ivan@gourame.com>
2020-10-24 17:13:07 +0100

    Delete unused

--- kontent-core/src/main/kotlin/io/github/kontent/asset/Assets.kt
+++ kontent-core/src/main/kotlin/io/github/kontent/asset/Assets.kt
@@ -8,8 +8,6 @@ class Assets(private val assets: List<Asset>) : Iterable<Asset> by assets {
 
     private val uriWithFingerprintsToAsset = assets.map { it.uriWithFingerprint.path to it }.toMap()
 
-    private val assetToFingerprint = uriWithFingerprintsToAsset.entries.map { (k, v) -> v.uri.path to k }.toMap()
-
     fun findByPath(path: String): Asset? = assets.find { it.uri.path == path }
 
     fun assetsWithFingerprint() = uriWithFingerprintsToAsset
